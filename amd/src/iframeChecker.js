a3f0894e1300bd4486cee009896d86adb97c89f7
Lai Wei <lai.wei@enovation.ie>
2018-10-08 16:56:34 +0100

    hide/show header/footer/blocks attempt 3

--- amd/src/iframeChecker.js
+++ amd/src/iframeChecker.js
@@ -1,13 +1,19 @@
-define(['jquery'], function ($) {
+define(['jquery', 'core/cfg'], function ($, cfg) {
     return {
         init: function() {
-            if ($(window).self !== $(window).top) {
+            console.log(cfg);
+            $('body').append('<img id="loadingicon" src="'+cfg.loadingicon+'" />');
+
+            if (window.location != window.parent.location) {
                 // in iframe
                 $("nav.navbar").hide();
                 $("div#nav-drawer").hide();
                 $("aside#block-region-side-pre").parent().hide();
                 $("footer#page-footer").hide();
             }
+
+            $("div#page").fadeIn(150);
+            $("img#loadingicon").fadeOut(150);
         }
     }
 });
