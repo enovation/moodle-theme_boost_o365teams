8cfa6faff0ca3aa04d2852f92b524d71192838a7
Lai Wei <lai.wei@enovation.ie>
2018-10-08 21:54:21 +0100

    update styling

--- amd/src/iframeChecker.js
+++ amd/src/iframeChecker.js
@@ -4,6 +4,7 @@ define(['jquery'], function ($) {
             if (window.location == window.parent.location) {
                 // in iframe
                 $("nav.navbar").show();
+                $("nav.navbar").setAttribute('display', 'flex');
                 $("div#nav-drawer").show();
                 $("aside#block-region-side-pre").parent().show();
                 $("footer#page-footer").show();
