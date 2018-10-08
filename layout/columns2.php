5c72abba2d3cfe613ae16da67e532c64462add5a
Lai Wei <lai.wei@enovation.ie>
2018-10-08 14:34:54 +0100

    add iframe checker to columns2 tempalte

---
+++ layout/columns2.php
@@ -0,0 +1,90 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * A two column layout for the boost theme.
+ *
+ * @package   theme_boost
+ * @copyright 2016 Damyon Wiese
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+user_preference_allow_ajax_update('drawer-open-nav', PARAM_ALPHA);
+require_once($CFG->libdir . '/behat/lib.php');
+
+if (isloggedin()) {
+    $navdraweropen = (get_user_preferences('drawer-open-nav', 'true') == 'true');
+} else {
+    $navdraweropen = false;
+}
+$extraclasses = [];
+if ($navdraweropen) {
+    $extraclasses[] = 'drawer-open-left';
+}
+$bodyattributes = $OUTPUT->body_attributes($extraclasses);
+$blockshtml = $OUTPUT->blocks('side-pre');
+$hasblocks = strpos($blockshtml, 'data-block=') !== false;
+$regionmainsettingsmenu = $OUTPUT->region_main_settings_menu();
+$templatecontext = [
+    'sitename' => format_string($SITE->shortname, true, ['context' => context_course::instance(SITEID), "escape" => false]),
+    'output' => $OUTPUT,
+    'sidepreblocks' => $blockshtml,
+    'hasblocks' => $hasblocks,
+    'bodyattributes' => $bodyattributes,
+    'navdraweropen' => $navdraweropen,
+    'regionmainsettingsmenu' => $regionmainsettingsmenu,
+    'hasregionmainsettingsmenu' => !empty($regionmainsettingsmenu)
+];
+
+$templatecontext['flatnavigation'] = $PAGE->flatnav;
+
+$js = '
+function inIframe () {
+    try {
+        return window.self !== window.top;
+    } catch (e) {
+        return true;
+    }
+}
+
+if (inIframe()) {
+    // if in iframe, hide navbar, nav-drawer, side-pre, and footer
+    let navbar = window.getElementsByTagName("nav")[0];
+    navbar.style.display = "none";
+    let navdrawer = window.getElementsById("nav-drawer")[0];
+    navdrawer.style.display = "none";
+    let sidepre = window.getElementsById("block-region-side-pre")[0];
+    sidepre.style.display = "none";
+    let footer = window.getElementsById("page-footer")[0];
+    footer.style.display = "none";
+} else {
+    // if not in iframe, show navbar, nav-drawer, side-pre, and footer
+    let navbar = window.getElementsByTagName("nav")[0];
+    navbar.style.display = "block";
+    let navdrawer = window.getElementsById("nav-drawer")[0];
+    navdrawer.style.display = "block";
+    let sidepre = window.getElementsById("block-region-side-pre")[0];
+    sidepre.style.display = "block";
+    let footer = window.getElementsById("page-footer")[0];
+    footer.style.display = "block";
+}
+';
+
+echo html_writer::script($js);
+
+echo $OUTPUT->render_from_template('theme_boost_o365teams/columns2', $templatecontext);
