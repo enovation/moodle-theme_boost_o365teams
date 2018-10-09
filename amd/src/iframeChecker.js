cef0535630a9193ff8608421a46a43c3aa4d2471
Lai Wei <lai.wei@enovation.ie>
2018-10-09 11:16:00 +0100

    update css selector

--- amd/src/iframeChecker.js
+++ amd/src/iframeChecker.js
@@ -11,12 +11,12 @@ define(['jquery'], function ($) {
             } else {
                 // in iframe, hide page elements
                 $('body.drawer-open-left').css('margin-left', '0');
-                $('#page').css('margin-top', '0');
-                $('#region-main.has-blocks').css('width', '100%');
-                $('#page-wrapper').css('margin-bottom', '0');
-                $('div#context-header-settings-menu').css('display', 'none');
-                $('div#region-main-settings-menu').css('display', 'none');
-                $('div#region_main_settings_menu_proxy').css('display', 'none');
+                $('div#page').css('margin-top', '0');
+                $('section#region-main.has-blocks').css('width', '100%');
+                $('div#page-wrapper').css('margin-bottom', '0');
+                $('div.context-header-settings-menu').css('display', 'none');
+                $('div.region-main-settings-menu').css('display', 'none');
+                $('div.region_main_settings_menu_proxy').css('display', 'none');
             }
 
             $("body").fadeIn(150);
