3b959224a9cd76d23aa1cab4506670fd3aeb129e
Lai Wei <lai.wei@enovation.ie>
2018-10-18 14:46:52 +0100

    update style of open course page link

--- classes/output/core_renderer.php
+++ classes/output/core_renderer.php
@@ -70,7 +70,7 @@ class core_renderer extends \theme_boost\output\core_renderer {
         if (!$link) {
             $courselink = new moodle_url('/course/view.php', array('id' => $COURSE->id));
             $link = html_writer::link($courselink, html_writer::empty_tag('img',
-                array('src' => $this->image_url('popupicon', 'theme'), 'class' => 'popupicon')),
+                array('src' => $this->image_url('popupicon_white', 'theme'), 'class' => 'popupicon_white')),
                 array('target' => '_blank', 'class' => 'btn btn-primary'));
         }
 
