149e81088df0a82f93830cbce44d5bef76cca358
Kevin Hipwell <kevin.hipwell@enovation.ie>
2018-10-22 14:15:12 +0100

    adding popup icon to all unsupported activities, setting unsupported activites to open in new window

--- amd/build/iframeChecker.js
+++ amd/build/iframeChecker.js
@@ -9,6 +9,7 @@ define(['jquery'], function ($) {
                 $('section[data-region="blocks-column"]').show();
                 $('footer#page-footer').show();
                 $('div#course_page_title').css('display', 'none');
+                $('.popupicon').css('display', 'none');
             } else {
                 // in iframe, hide page elements
                 $('body.drawer-open-left').css('margin-left', '0');
@@ -23,6 +24,18 @@ define(['jquery'], function ($) {
                 $('div.ml-auto').remove();
                 $('a.printicon').remove();
                 $('header#page-header').css('display', 'none');
+                $('.activityinstance a').click(function() {
+                    $(this).attr('target', '_blank');
+                });
+                $('.modtype_assign .activityinstance a').click(function() {
+                    $(this).attr('target', '_self');
+                });
+                $('.modtype_quiz .activityinstance a').click(function() {
+                    $(this).attr('target', '_self');
+                });
+                $('.modtype_forum .activityinstance a').click(function() {
+                    $(this).attr('target', '_self');
+                });
             }
 
             $("body").fadeIn(150);
